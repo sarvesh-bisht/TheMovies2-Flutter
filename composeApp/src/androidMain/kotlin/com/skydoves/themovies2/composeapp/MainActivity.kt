b5a0ad0baf6aba4648daf56c2fb4ded202e4cb5d
Sarvesh Bisht <sarveshbisht1987@gmail.com>
2026-03-04 16:24:09 +0530

    Set composeApp system bars to black for parity

--- composeApp/src/androidMain/kotlin/com/skydoves/themovies2/composeapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/skydoves/themovies2/composeapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.skydoves.themovies2.composeapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,8 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    window.statusBarColor = Color.BLACK
+    window.navigationBarColor = Color.BLACK
     setContent { App() }
   }
 }
